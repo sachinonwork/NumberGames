c8e76a4333c52e50ef590b192f20327098b2e730
sdanayak <sachin.danayak@gmail.com>
2019-10-23 23:49:35 +0530

    Added test for negative numbers to be handled

--- NumberGames/src/main/java/co/in/numbers/StringCalculator.java
+++ NumberGames/src/main/java/co/in/numbers/StringCalculator.java
@@ -1,22 +1,27 @@
 package co.in.numbers;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class StringCalculator {
 
     private String[] stringToCheck = {StringUtils.CR, StringUtils.LF, ";", ":"};
 
-    public int add(String param) {
+    public int add(String param) throws Exception {
         if (StringUtils.isBlank(param)) {
             return 0;
         }
+        List<Integer> errorList = new ArrayList<Integer>();
         int totalOfAll = 0;
         StringTokenizer paramStringToTotal = new StringTokenizer(param, ",");
         while (paramStringToTotal.hasMoreTokens()) {
             String numberToAdd = replaceStringInListIfAny(paramStringToTotal);
-            totalOfAll += Integer.valueOf(numberToAdd);
+            int number = Integer.valueOf(StringUtils.trim(numberToAdd));
+            totalOfAll += number;
         }
         return totalOfAll;
     }
